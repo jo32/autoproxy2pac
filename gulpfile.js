b88b906b06fbbcd4f1d18899fdf811d7c6e4c5a8
JIANG DL <j.jolam@gmail.com>
2015-09-30 11:26:02 +0800

    add bin

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var insert = require('gulp-insert');
 
 gulp.task('default', function() {
-    gulp.src('./src/**/*.js').pipe(babel({
+    gulp.src('./src/**/*.js')
+        .pipe(babel({
         optional: ['runtime']
-    })).pipe(gulp.dest('./build'));
+    }))
+        .pipe(insert.prepend('#!/usr/bin/env node\n'))
+        .pipe(gulp.dest('./build'));
 });
